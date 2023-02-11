de37ec156947d9827545c71da56c6737017ab4e9
Boris Kogan <fierval@gmail.com>
2023-02-11 01:37:36 +0000

    train runner

---
+++ train/run.sh
@@ -0,0 +1,8 @@
+mpirun -np 8 \
+-H 104.171.203.48 \
+-x MASTER_ADDR=104.171.203.48 \
+-x MASTER_PORT=1234 \
+-x PATH \
+-bind-to none -map-by slot \
+-mca pml ob1 -mca btl ^openib \
+python3 train.py -d "soul11zz/image-caption-desc-only" -m "soul11zz/image-caption-desc-only" -b 8 -e 1 --test-best --metric meteor
