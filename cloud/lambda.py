f7a7492ceca0ecf040848d7b86513bf37bf50a74
Boris <fierval@gmail.com>
2023-02-11 18:23:22 -0800

    command line arguments

--- cloud/lambda.py
+++ cloud/lambda.py
@@ -8,10 +8,10 @@ from argparse import ArgumentParser
 def parse_args():
   parser = ArgumentParser()
 
-  parser.add_argument("--run-sh-path", "-i", required=True, help="Path to run.sh")
+  parser.add_argument("--run-sh-path", "-r", required=True, help="Path to run.sh")
   parser.add_argument("--config-sh-path", "-c", required=True, help="Path to config.sh")
-  parser.add_argument("--ip-list",  required=True, help="List of IPs to use, comma separated (first in the list is the master)")
-  parser.add_argument("--gpu-list",  required=True, help="List of GPUs/IP to use, comma separated")
+  parser.add_argument("--ip-list", "-i", required=True, help="List of IPs to use, comma separated (first in the list is the master)")
+  parser.add_argument("--gpu-list", "-g", required=True, help="List of GPUs/IP to use, comma separated")
   
   return parser.parse_args()
 
